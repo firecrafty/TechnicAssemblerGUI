ca89309a5652e1a0e9322a9bfb2d196bd6e1c182
firecrafty <bluer2288@students.pike.k12.in.us>
2016-04-12 11:24:56 -0400

    Try to bundle mac app

--- src/main/java/com/github/firecrafty/technicassemblergui/MainWindow.java
+++ src/main/java/com/github/firecrafty/technicassemblergui/MainWindow.java
@@ -6,6 +6,8 @@ import com.github.firecrafty.technicassembler.ZipFile;
 import com.github.firecrafty.technicassembler.logging.SimpleLogger;
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 /**
  *
@@ -35,17 +37,21 @@ public class MainWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         fileChooser = new javax.swing.JFileChooser();
-        modpackNameField = new javax.swing.JTextField();
-        modpackNameLabel = new javax.swing.JLabel();
-        modpackVersionField = new javax.swing.JTextField();
-        modpackVersionLabel = new javax.swing.JLabel();
-        modpackFolderButton = new javax.swing.JButton();
+        jOptionPane1 = new javax.swing.JOptionPane();
+        modpackDataPanel = new javax.swing.JPanel();
         folderText = new javax.swing.JTextField();
         folderLabel = new javax.swing.JLabel();
+        modpackVersionLabel = new javax.swing.JLabel();
+        modpackVersionField = new javax.swing.JTextField();
+        modpackNameField = new javax.swing.JTextField();
+        modpackNameLabel = new javax.swing.JLabel();
+        modpackFolderButton = new javax.swing.JButton();
+        programControlsPanel = new javax.swing.JPanel();
+        statusLabel = new javax.swing.JLabel("", SwingConstants.CENTER);
         submit = new javax.swing.JButton();
-        clientBox = new javax.swing.JCheckBox();
+        sidesPanel = new javax.swing.JPanel();
         serverBox = new javax.swing.JCheckBox();
-        statusLabel = new javax.swing.JLabel();
+        clientBox = new javax.swing.JCheckBox();
 
         fileChooser.setCurrentDirectory(TechnicAssembler.getWorkingDirectory());
         fileChooser.setFileSelectionMode(javax.swing.JFileChooser.DIRECTORIES_ONLY);
@@ -54,15 +60,22 @@ public class MainWindow extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("TechnicAssembler");
         setName("MainFrame"); // NOI18N
+        setResizable(false);
+
+        folderText.setEditable(false);
+        folderText.setText(TechnicAssembler.getWorkingDirectory().toString());
+
+        folderLabel.setText("Modpack Folder");
+        folderLabel.setToolTipText("Defaults to current directory");
+
+        modpackVersionLabel.setText("Modpack Version");
+
+        modpackVersionField.setText("0.0.0");
 
         modpackNameField.setText("Modpack");
         modpackNameField.setToolTipText("");
 
         modpackNameLabel.setText("Modpack Name");
-
-        modpackVersionField.setText("0.0.0");
-
-        modpackVersionLabel.setText("Modpack Version");
 
         modpackFolderButton.setText("Browse");
         modpackFolderButton.addActionListener(new java.awt.event.ActionListener() {
@@ -71,24 +84,51 @@ public class MainWindow extends javax.swing.JFrame {
             }
         });
 
-        folderText.setEditable(false);
-        folderText.setText(TechnicAssembler.getWorkingDirectory().toString());
+        javax.swing.GroupLayout modpackDataPanelLayout = new javax.swing.GroupLayout(modpackDataPanel);
+        modpackDataPanel.setLayout(modpackDataPanelLayout);
+        modpackDataPanelLayout.setHorizontalGroup(
+            modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(modpackDataPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(modpackNameLabel)
+                    .addComponent(modpackVersionLabel)
+                    .addComponent(folderLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(modpackNameField, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(modpackVersionField, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(modpackDataPanelLayout.createSequentialGroup()
+                        .addComponent(folderText, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(modpackFolderButton)))
+                .addContainerGap())
+        );
+        modpackDataPanelLayout.setVerticalGroup(
+            modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, modpackDataPanelLayout.createSequentialGroup()
+                .addContainerGap(33, Short.MAX_VALUE)
+                .addGroup(modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(modpackNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(modpackNameLabel))
+                .addGap(18, 18, 18)
+                .addGroup(modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(modpackVersionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(modpackVersionLabel))
+                .addGap(18, 18, 18)
+                .addGroup(modpackDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(folderText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(folderLabel)
+                    .addComponent(modpackFolderButton))
+                .addContainerGap())
+        );
 
-        folderLabel.setText("Modpack Folder");
-        folderLabel.setToolTipText("Defaults to current directory");
+        statusLabel.setText("Ready");
 
         submit.setText("Zip Modpack");
         submit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 submitActionPerformed(evt);
-            }
-        });
-
-        clientBox.setSelected(buildClient);
-        clientBox.setText("Build Client");
-        clientBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                clientBoxActionPerformed(evt);
             }
         });
 
@@ -100,64 +140,81 @@ public class MainWindow extends javax.swing.JFrame {
             }
         });
 
-        statusLabel.setText("Ready");
+        clientBox.setSelected(buildClient);
+        clientBox.setText("Build Client");
+        clientBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clientBoxActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout sidesPanelLayout = new javax.swing.GroupLayout(sidesPanel);
+        sidesPanel.setLayout(sidesPanelLayout);
+        sidesPanelLayout.setHorizontalGroup(
+            sidesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(sidesPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(serverBox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(clientBox)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        sidesPanelLayout.setVerticalGroup(
+            sidesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(sidesPanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(sidesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(serverBox)
+                    .addComponent(clientBox)))
+        );
+
+        javax.swing.GroupLayout programControlsPanelLayout = new javax.swing.GroupLayout(programControlsPanel);
+        programControlsPanel.setLayout(programControlsPanelLayout);
+        programControlsPanelLayout.setHorizontalGroup(
+            programControlsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(statusLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(programControlsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(programControlsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(submit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(programControlsPanelLayout.createSequentialGroup()
+                        .addComponent(sidesPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        programControlsPanelLayout.setVerticalGroup(
+            programControlsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, programControlsPanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(sidesPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(statusLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(submit, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(modpackNameLabel)
-                    .addComponent(modpackVersionLabel)
-                    .addComponent(folderLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(modpackNameField, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(modpackVersionField, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(folderText, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(modpackFolderButton)))
-                .addGap(70, 70, 70))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(modpackDataPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30))
             .addGroup(layout.createSequentialGroup()
-                .addGap(57, 57, 57)
-                .addComponent(clientBox)
-                .addGap(62, 62, 62)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(submit, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(serverBox)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(53, 53, 53)
-                        .addComponent(statusLabel)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(programControlsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(modpackNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(modpackNameLabel))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(modpackVersionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(modpackVersionLabel))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(folderText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(modpackFolderButton)
-                    .addComponent(folderLabel))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(clientBox)
-                    .addComponent(serverBox))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 328, Short.MAX_VALUE)
-                .addComponent(statusLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(submit)
-                .addGap(35, 35, 35))
+                .addContainerGap()
+                .addComponent(modpackDataPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(programControlsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -195,18 +252,16 @@ public class MainWindow extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         logger.startLog();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() { logger.stopLog(); }
+        });
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
@@ -224,7 +279,6 @@ public class MainWindow extends javax.swing.JFrame {
                 new MainWindow().setVisible(true);
             }
         });
-        logger.stopLog();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -232,12 +286,16 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JFileChooser fileChooser;
     private javax.swing.JLabel folderLabel;
     private javax.swing.JTextField folderText;
+    private javax.swing.JOptionPane jOptionPane1;
+    private javax.swing.JPanel modpackDataPanel;
     private javax.swing.JButton modpackFolderButton;
     private javax.swing.JTextField modpackNameField;
     private javax.swing.JLabel modpackNameLabel;
     private javax.swing.JTextField modpackVersionField;
     private javax.swing.JLabel modpackVersionLabel;
+    private javax.swing.JPanel programControlsPanel;
     private javax.swing.JCheckBox serverBox;
+    private javax.swing.JPanel sidesPanel;
     private javax.swing.JLabel statusLabel;
     private javax.swing.JButton submit;
     // End of variables declaration//GEN-END:variables
